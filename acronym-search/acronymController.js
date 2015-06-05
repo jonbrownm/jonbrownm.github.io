6957da39d8bdeb4cdc6d5ef4ef7e221946bf79b0
Jonathan Brown <jonathan.brown@bt.com>
2015-06-05 12:43:08 +0100

    Fixed result status and added ng-cloak

--- acronym-search/acronymController.js
+++ acronym-search/acronymController.js
@@ -1,10 +1,4 @@
 app.controller('acronymCtrl', function($scope, $http) {
     $http.get("data.json")
     .success(function(response) {$scope.acronyms = response.records;});
-    $scope.filterKeywordsLength = 5;
-
-    $scope.viewMoreResults = function() {
-
-    };
-
 });
