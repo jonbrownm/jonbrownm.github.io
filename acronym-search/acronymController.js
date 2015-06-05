fc2450453afad8af2c1f0e9699858b0472994413
Jonathan Brown <jonathan.brown@bt.com>
2015-06-05 11:33:23 +0100

    Changed names of variables

--- acronym-search/acronymController.js
+++ acronym-search/acronymController.js
@@ -1,7 +1,7 @@
 app.controller('acronymCtrl', function($scope, $http) {
     $http.get("data.json")
     .success(function(response) {$scope.acronyms = response.records;});
-    $scope.filterNo = 5;
+    $scope.filterKeywordsLength = 5;
 
     $scope.viewMoreResults = function() {
 
