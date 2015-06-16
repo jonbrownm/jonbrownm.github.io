d0ef7d80739a3865c0965c0f7a1414d9f96f5286
Jonathan Brown <jonathan.brown@bt.com>
2015-06-16 12:21:28 +0100

    Refactored JS to remove duplication

--- list-manager/javascript.js
+++ list-manager/javascript.js
@@ -1,45 +1,33 @@
 
 $( document ).ready(function() {
-
-	for (var i = 0; i < localStorage.length; i++){
-
-	    key = localStorage.key(i);
-		keyInit = key.substring(0, 2)
-
-		if (keyInit == "lm") {
-			itemText = localStorage.getItem(key);
-			$("table[data-list] tbody").append("<tr data-list-id='" + key + "'><td>" + itemText + "</td><td><button class='btn btn-danger btn-xs pull-right' data-list-delete type='submit'>Delete</button></td></tr>")
-		}
-
-	}
-
-	postUpdate();
+	updateTable();
 
 	$("input[data-list]").keypress(function(e){
-	    if(e.which == 13){
-	    	
+	    if(e.which == 13){	
 	        itemText = $("input[data-list]").val();
-	        itemId = "lm" + Math.floor((Math.random() * 999) + 1);
+	        itemId = "_" + Math.floor((Math.random() * 999) + 1);
 	        localStorage.setItem(itemId, itemText);
-	        $("table[data-list] tbody").append("<tr data-list-id='" + itemId + "'><td>" + itemText + "</td><td><button class='btn btn-danger btn-xs pull-right' data-list-delete type='submit'>Delete</button></td></tr>")
-	        $("input[data-list]").val("");
-	        postUpdate();
-	        
+	        updateTable();
 	    }
 	});
 
 	$("table[data-list] tbody").on("click", "button[data-list-delete]", function(){
-
 		itemId = $(this).parent().parent().attr("data-list-id");
-
 		localStorage.removeItem(itemId);
-		$("table[data-list] tbody tr[data-list-id='" + itemId + "']").remove();
-		postUpdate();
-
+		updateTable();
 	});
 	
 });
 
-function postUpdate() {
-
+function updateTable() {
+	$("input[data-list]").val("");
+	$("table[data-list] tbody").empty();
+	for (var i = 0; i < localStorage.length; i++){
+	    key = localStorage.key(i);
+		keyInit = key.substring(0, 1)
+		if (keyInit == "_") {
+			itemText = localStorage.getItem(key);
+			$("table[data-list] tbody").append("<tr data-list-id='" + key + "'><td>" + itemText + "</td><td><button class='btn btn-danger btn-xs pull-right' data-list-delete type='submit'>Delete</button></td></tr>")
+		}
+	}
 }
