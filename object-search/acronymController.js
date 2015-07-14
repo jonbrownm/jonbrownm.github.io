c30e3b420f1d44da701bce7b002e110d222fd0d9
Jonathan Brown <jonathan.brown@bt.com>
2015-07-14 10:02:12 +0100

    Fixed postcode validation

--- object-search/acronymController.js
+++ object-search/acronymController.js
@@ -6,16 +6,26 @@ app.controller('acronymCtrl', function($scope, $http) {
     $scope.myData = data ;
   });
   
-  $scope.findValue = function(enteredValue) {     
+  $scope.findValue = function(enteredValue) {
+
+    enteredValue = enteredValue.replace(/\s/g, "");
+    enteredValue = enteredValue.toUpperCase();
+
+    var regex = /[A-Za-z]{1,2}[0-9Rr][0-9A-Za-z]?[0-9][ABD-HJLNP-UW-Zabd-hjlnp-uw-z]{2}/i;
+    isPostcode = regex.test(enteredValue);
+
     angular.forEach($scope.myData.Postcode, function(value, key) {
 
-      enteredValue = enteredValue.replace(" ",""); 
-      enteredValue = enteredValue.toUpperCase();
-
-      if (key.indexOf(enteredValue) >= 0) {
+      if (isPostcode == true && key.indexOf(enteredValue) >= 0) {
         $scope.results = [];
         $scope.results.push({postcode: key, business: value[0].business, url: value[0].url});
       }
+      else {
+        $scope.results = [];        
+      }
+
     });
+
   };
+
 });
