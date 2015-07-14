678c1c0b8586fa6a39ae425c7b12a0ec4ec4291e
Jonathan Brown <jonathan.brown@bt.com>
2015-07-14 10:50:58 +0100

    Updated data to london postcodes

--- object-search/acronymController.js
+++ object-search/acronymController.js
@@ -11,17 +11,14 @@ app.controller('acronymCtrl', function($scope, $http) {
     enteredValue = enteredValue.replace(/\s/g, "");
     enteredValue = enteredValue.toUpperCase();
 
-    var regex = /[A-Za-z]{1,2}[0-9Rr][0-9A-Za-z]?[0-9][ABD-HJLNP-UW-Zabd-hjlnp-uw-z]{2}/i;
-    isPostcode = regex.test(enteredValue);
+    //var regex = /[A-Za-z]{1,2}[0-9Rr][0-9A-Za-z]?[0-9][ABD-HJLNP-UW-Zabd-hjlnp-uw-z]{2}/i;
+    //isPostcode = regex.test(enteredValue);
 
     angular.forEach($scope.myData.Postcode, function(value, key) {
 
-      if (isPostcode == true && key.indexOf(enteredValue) >= 0) {
+      if (key.indexOf(enteredValue) >= 0) {
         $scope.results = [];
         $scope.results.push({postcode: key, business: value[0].business, url: value[0].url});
-      }
-      else {
-        $scope.results = [];        
       }
 
     });
