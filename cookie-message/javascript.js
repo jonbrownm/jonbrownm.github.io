8b4acabfe88f6964705b01c589a3915bc842052e
Jonathan Brown <jonathan.brown@bt.com>
2017-03-02 10:48:55 +0000

    Updated cookie js

--- cookie-message/javascript.js
+++ cookie-message/javascript.js
@@ -1,24 +1,42 @@
-window.onload = function(){
-	var name=getCookie("notification");
-    if (name != "") {
-        document.getElementById("notification").style.visibility = "hidden";
-    } else {
-        document.getElementById("notification").style.visibility = "visible";   	
-    }
-}
+$(document).ready(function() {
 
-function setCookie(cname,cvalue,exdays) {
+    cookieName = $(".alert").data("cookie");
+
+	var name = getCookie(cookieName);
+
+    if (name != "") {
+        $(".alert[data-cookie='" + cookieName + "']").hide();
+    } else {
+        $(".alert[data-cookie='" + cookieName + "']").show();
+    }
+
+});
+
+
+$(".alert button").click(function(event) {
+
+    event.preventDefault();
+
+    cname = $(this).parent().attr("data-cookie");
+    cvalue = true;
+    exdays = 365;
+
     var d = new Date();
     d.setTime(d.getTime() + (exdays*24*60*60*1000));
     var expires = "expires=" + d.toGMTString();
+
     document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
-    document.getElementById("notification").style.visibility = "hidden";
-}
+    
+    $(".alert[data-cookie='" + cname + "']").hide();
+
+});
 
 function getCookie(cname) {
+
     var name = cname + "=";
     var decodedCookie = decodeURIComponent(document.cookie);
     var ca = decodedCookie.split(';');
+
     for(var i = 0; i < ca.length; i++) {
         var c = ca[i];
         while (c.charAt(0) == ' ') {
@@ -28,5 +46,6 @@ function getCookie(cname) {
             return c.substring(name.length, c.length);
         }
     }
+    
     return "";
 }
