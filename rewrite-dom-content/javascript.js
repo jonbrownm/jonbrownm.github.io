5233907ef48c78cfe1cbf76d2990038054701636
Jonathan Brown <jonathan.brown@bt.com>
2017-07-21 11:35:06 +0100

    Added rewrite the dom

--- rewrite-dom-content/javascript.js
+++ rewrite-dom-content/javascript.js
@@ -1,26 +1,26 @@
 (function() {
 
-  // calculate current date
-  var currentTime = new Date();
-  currentYear = currentTime.getFullYear();
+	// calculate current date
+	var currentTime = new Date();
+	currentYear = currentTime.getFullYear();
 
-  var config = {
-    att: "data-rewrite-year",
-    string: currentYear
-  },
+	var config = {
+		att: "data-rewrite-year",
+		string: currentYear
+	},
 
-  rewriteDomContent = function(element) {
-    element.replaceWith(config.string);
-  },
-  
+	rewriteDomContent = function(element) {
+		element.replaceWith(config.string);
+	},
+	
 	init = function() {		
-    elements = document.querySelectorAll("[" + config.att + "]");
+		elements = document.querySelectorAll("[" + config.att + "]");
 		
-    for (var i = 0; i < elements.length; i++) {
-    	rewriteDomContent(elements[i]);
+		for (var i = 0; i < elements.length; i++) {
+			rewriteDomContent(elements[i]);
 		}
 	};
-  
-  init()
+	
+	init()
 
 })();
